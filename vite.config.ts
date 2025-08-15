717d65f0e903874e1a917adb1e1caee9972678d4
zihao <jiangzihaoalex@gmail.com>
2025-08-15 00:01:00 -0400

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { Buffer } from "buffer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -21,7 +20,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   define: {
-    'global': {},
-    'Buffer': Buffer,
+    'global': 'globalThis',
   }
 }));
